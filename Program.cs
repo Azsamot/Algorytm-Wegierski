000089308be6359b92984ff217cb7cfb11c9dbba
Azsamot <31781572+Azsamot@users.noreply.github.com>
2018-05-03 14:14:09 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ namespace Grafy_i_Sieci
 {
     class Program
     {
+        private static bool CzyKwadratowa(uint[,] macierz)
+        {
+            return macierz.GetLength(0) == macierz.GetLength(1);
+        }
+        
         static void Main(string[] args)
         {
             uint[,] macierz = new uint[,] { { 3, 3, 3 }, { 2, 2, 2 }, { 1, 1, 1 } };
